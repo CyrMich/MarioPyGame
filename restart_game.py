12a473e31b3219170712d4c355c96603f6b9fb75
CyrMich <mcyrangod@gmail.com>
2025-06-10 12:44:02 +0200

    Update restart_game.py

--- restart_game.py
+++ restart_game.py
@@ -5,7 +5,7 @@ import constants
 
 def restart_game():
     lives = DIFFICULTY_SETTINGS[constants.CURRENT_DIFFICULTY]["player_lives"]
-    player = Player(400, FLOOR_LEVEL, 50, 50, lives)
+    player = Player(100, FLOOR_LEVEL, 50, 50, lives)
     speed_mult = DIFFICULTY_SETTINGS[constants.CURRENT_DIFFICULTY]["enemy_speed_multiplier"]
     enemies = [
         Goomba(700, FLOOR_LEVEL + 12, 45, 45, int(3 * speed_mult) + 1, 750),
